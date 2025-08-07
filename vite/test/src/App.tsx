ae7032d51d5ee3f59ae0c021c58cf97bece29cf4
xayliot <megaponch2004@gmail.com>
2025-08-07 17:59:14 +0300

    tasks: sidebar, login/signin, tailwind_tests

---
+++ vite/test/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './login';
+import Register from './register';
+
+function App(){
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ vite/test/src/login.tsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+
+
+function Login() {
+  return (
+    <>
+<div className="flex flex-1 min-h-full flex-col justify-center px-6 py-12 lg:px-8 darkMode dark:bg-[#101828]">
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" className="mx-auto h-10 w-auto" />
+    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-white">Sign in to your account</h2>
+  </div>
+
+  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm ">
+    <form action="#" method="POST" className="space-y-6">
+      <div>
+        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900 dark:text-white">Email address</label>
+        <div className="mt-2">
+          <input id="email" type="email" name="email" required placeholder="email" className="block w-full rounded-md bg-white dark:bg-[#1C2433] px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900 dark:text-white">Password</label>
+          <div className="text-sm">
+            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+          </div>
+        </div>
+        <div className="mt-2">
+          <input id="password" type="password" name="password" required placeholder="current-password" className="block w-full rounded-md bg-white dark:bg-[#1C2433] px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+      </div>
+    </form>
+
+    <p className="mt-10 text-center text-sm/6 text-gray-500">
+      Not a member?
+              <Link to="/Register" className="font-semibold text-indigo-600 hover:text-indigo-500">
+          Register in now
+        </Link>
+    </p>
+  </div>
+</div>
+    </>
+  )
+}
+
+export default Login  
